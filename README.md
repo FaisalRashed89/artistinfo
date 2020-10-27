a15ef67fe40eb3143630b8368fe98791657a2b9b
Faisal Alotaibi <f.f0001989@gmail.com>
2020-10-27 15:40:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 📊 Project: Complex API 2
+
+1. The requirements were to make request to 2 separate APIs and combine the results on a webpage.
+
+2. The project meets the requirements by using client side Javascript to fetch data from the album search API found at http://ws.audioscrobbler.com to return albums and bios of the artist supplied by the user. Then, it uses the GIF API found at https://api.giphy.com to fetch an image featuring the artist. Then the program places the combined output of the two APIs into the page.
+
+3. The project uses the fetch function to make calls to the APIs, and then places the results on the page using document.querySelector to find the HTML elements in which to insert the content into the DOM.
+
+4. I wrote the code myself with the help of Resilient Coders mentors.
+
+5. I would improve the UX/UI design by using CSS to change the layout of the page, the colors, fonts, and spacing. I would also work on beautifying the input and the button contained in the form where the user enters and submits the artist's name to the Javascript code. In the Javascript code I would add a conditional to clear the GIF if one is not found for the artist.
+
+6. This project is purely client side HTML, CSS, and Javascript and therefore it does not require a web server. You may download the files from Github in the form of a zip file and open the index.html file in your browser. Alternatively, one could set up a web server and configure it to serve these files via the HTTP protocol.
---
+++ main.js
@@ -0,0 +1,48 @@
+// Find the form element in the page
+let form = document.querySelector('form');
+//Listen for submit event on the form and display artisit info
+form.addEventListener('submit',displayArtistInfo);
+
+// API Keys
+let giphyApiKey ='AoXagPnxpYhzo2HWzj3KaWNTZkwVZ8io';
+let scrobblerApiKey ='0a9eb4523d8b519bfda857a49973daeb';
+
+// Function to display artist info
+function displayArtistInfo(e) {
+
+// Prevent a link from opening the URL
+  e.preventDefault();
+
+// Get the input elment from the HTML and store it in the artistInput variable
+  let artistInput = document.querySelector("input");
+
+// Get the value from the input and store it in the artistName variable
+  let artistName = artistInput.value;
+
+// this fetch gets and displays the artist album covers
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${artistName}&api_key=${scrobblerApiKey}&format=json`)
+  .then(res => res.json()) //convert result to json
+  .then(response => { //process the json
+    console.log(response)
+    document.querySelector('h2').innerHTML = response.results.albummatches.album[0].artist
+    document.querySelector('#albumCoverOne').src = response.results.albummatches.album[0].image[3]["#text"]
+    document.querySelector('#albumCoverTwo').src = response.results.albummatches.album[1].image[3]["#text"]
+    document.querySelector('#albumCoverThree').src = response.results.albummatches.album[2].image[3]["#text"]
+    document.querySelector('#albumCoverFour').src = response.results.albummatches.album[3].image[3]["#text"]
+  });
+
+//this fetch gets and displays the artist bio
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=${scrobblerApiKey}&format=json`)
+  .then(res => res.json())
+  .then(response => {
+    document.querySelector('#artistBio').textContent = response.artist.bio.content
+  });
+
+//this fetch gets and displays a Gif of the artist
+  fetch(`https://api.giphy.com/v1/gifs/search?q=${artistName}&api_key=${giphyApiKey}&limit=5`)
+  .then(res => res.json())
+  .then(json => {
+    console.log("image", json.data[0].images.original.url);
+    document.querySelector('#artistImage').src= json.data[0].images.original.url
+  });
+}
